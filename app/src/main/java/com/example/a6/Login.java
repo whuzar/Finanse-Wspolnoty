5342f717704c842b44da56361a1b9ece2ff910e7
Krystian <krystek.sadowski@gmail.com>
2022-10-09 18:35:41 +0200

    [add] fix action bar

--- app/src/main/java/com/example/a6/Login.java
+++ app/src/main/java/com/example/a6/Login.java
@@ -29,7 +29,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
 //        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
         final TextInputLayout logi = findViewById(R.id.log);
--- app/src/main/java/com/example/a6/MainActivity.java
+++ app/src/main/java/com/example/a6/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
