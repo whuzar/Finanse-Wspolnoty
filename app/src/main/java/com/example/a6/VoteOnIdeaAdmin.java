045d9b9692ee785c1150950e8f0cf8b8dd36b51b
wojte <wojtekhuzar@gmail.com>
2022-10-11 21:32:02 +0200

    rozpoczecie do wysylania danych glosowanie

--- app/src/main/java/com/example/a6/VoteOnIdeaAdmin.java
+++ app/src/main/java/com/example/a6/VoteOnIdeaAdmin.java
@@ -4,31 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 
 public class VoteOnIdeaAdmin extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private TextView dateText;
-
+    private EditText valuefor, votetheme;
+    private Button btndate, sendvote;
+    private RadioButton vt1, vt2, valuer1, valuer2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote_on_idea_admin);
-        dateText = findViewById(R.id.cos);
 
-        findViewById(R.id.choosedate).setOnClickListener(new View.OnClickListener() {
+        dateText = findViewById(R.id.cos);
+        btndate = findViewById(R.id.choosedate);
+        valuefor = findViewById(R.id.showedit);
+        valuer2 = findViewById(R.id.radiochoice2value);
+        valuer1 = findViewById(R.id.radiochoice1value);
+        sendvote = findViewById(R.id.senddatavote);
+        votetheme = findViewById(R.id.themevote);
+
+        final String themevt = votetheme.getText().toString();
+        System.out.println(themevt);
+
+        btndate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog();
             }
         });
 
+        valuer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((RadioButton) v).isChecked();
+                if (checked){
+                    valuefor.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        valuer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((RadioButton) v).isChecked();
+                if (checked){
+                    valuefor.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        sendvote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(VoteOnIdeaAdmin.this, themevt, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void showDatePickerDialog(){
