5b52ed1529ffb20e6e6d5aebc85fcc8f57116245
wojte <wojtekhuzar@gmail.com>
2022-10-18 20:53:40 +0200

    popup in contact admin

--- app/src/main/java/com/example/a6/myadapterrec.java
+++ app/src/main/java/com/example/a6/myadapterrec.java
@@ -1,12 +1,16 @@
 package com.example.a6;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,10 +18,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.makeramen.roundedimageview.RoundedTransformationBuilder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class myadapterrec extends FirebaseRecyclerAdapter<modelrec, myadapterrec.myviewrec> {
+
+    List<String> name=new ArrayList<String>();
 
     public myadapterrec(@NonNull FirebaseRecyclerOptions<modelrec> options) {
         super(options);
@@ -27,13 +39,14 @@ public class myadapterrec extends FirebaseRecyclerAdapter<modelrec, myadapterrec
     @Override
     protected void onBindViewHolder(@NonNull myviewrec holder, int position, @NonNull modelrec model) {
 
-
         holder.namesur.setText(model.getName() + " " + model.getSurname());
         holder.email.setText(model.getEmail());
         holder.phone.setText("+48" + model.getPhone());
         if(model.getPimage() == null){
+            name.add(null);
         }else {
             Glide.with(holder.img.getContext()).load(model.getPimage()).into(holder.img);
+            name.add(model.getPimage());
         }
 
     }
@@ -43,19 +56,22 @@ public class myadapterrec extends FirebaseRecyclerAdapter<modelrec, myadapterrec
     public myviewrec onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerowrec, parent, false);
         myviewrec viewHolder = new myviewrec(view);
+
         viewHolder.line.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(parent.getContext(), "Test Click"+String.valueOf(viewHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                ShowPopup(view, parent, viewHolder.phone, viewHolder.email, viewHolder.getAdapterPosition());
+
             }
         });
+
         return viewHolder;
     }
 
     class myviewrec extends RecyclerView.ViewHolder{
 
         private CircleImageView img;
-        private TextView namesur, phone, email;
+        public TextView namesur, phone, email;
         LinearLayout line;
 
         public myviewrec(@NonNull View intemview){
@@ -68,5 +84,49 @@ public class myadapterrec extends FirebaseRecyclerAdapter<modelrec, myadapterrec
             email = itemView.findViewById(R.id.emailrec);
 
         }
+    }
+    public void ShowPopup(View v, ViewGroup parent, TextView t1, TextView t2, int i1){
+        Dialog mDialog;
+        mDialog = new Dialog(parent.getContext());
+        mDialog.setContentView(R.layout.popupcontactadmin);
+        ImageView call = mDialog.findViewById(R.id.call);
+        ImageView mail = mDialog.findViewById(R.id.sendmail);
+        ImageView profpopup = mDialog.findViewById(R.id.photoprofilepopup);
+        String emailmail = String.valueOf(t2.getText());
+        String phonephon = String.valueOf(t1.getText());
+        String linkprofil = name.get(i1);
+
+        if(linkprofil != null){
+            Transformation transformation = new RoundedTransformationBuilder()
+                    .borderColor(Color.WHITE)
+                    .borderWidthDp(1)
+                    .cornerRadiusDp(100)
+                    .oval(true)
+                    .build();
+
+            Picasso.get()
+                    .load(linkprofil)
+                    .fit()
+                    .transform(transformation)
+                    .into(profpopup);
+        }
+
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW
+                        , Uri.parse("mailto:" + emailmail));
+                parent.getContext().startActivity(intent);
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW
+                        , Uri.parse("tel:" + phonephon));
+                parent.getContext().startActivity(intent);
+            }
+        });
+        mDialog.show();
     }
 }
